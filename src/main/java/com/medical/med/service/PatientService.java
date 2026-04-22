789c38626b51fb88069646188c22b00898c72e08
jjet220 <daniilvukin62@gmail.com>
2026-04-22 10:11:05 +0500

    Update PatientService.java

--- src/main/java/com/medical/med/service/PatientService.java
+++ src/main/java/com/medical/med/service/PatientService.java
@@ -1,9 +1,7 @@
 package com.medical.med.service;
 
 import com.medical.med.model.Patient;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public interface PatientService {
     Patient findPatientById(Long patientId);
 }
