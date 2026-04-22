571e1207dcc0dc86729658a24f765e2d97cabca8
jjet220 <daniilvukin62@gmail.com>
2026-04-22 10:10:52 +0500

    Update PatientServiceImpl.java

--- src/main/java/com/medical/med/service/impl/PatientServiceImpl.java
+++ src/main/java/com/medical/med/service/impl/PatientServiceImpl.java
@@ -3,11 +3,9 @@ package com.medical.med.service.impl;
 import com.medical.med.model.Patient;
 import com.medical.med.repository.PatientRepository;
 import com.medical.med.service.PatientService;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary
 public class PatientServiceImpl implements PatientService {
     private final PatientRepository patientRepository;
 
